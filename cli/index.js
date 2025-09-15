ed5d8e9c020cf6486ec09f83e29dcb5a04aac802
skyphrax <skyphraxoff@gmail.com>
2025-09-15 11:24:56 +0200

    Fix uuid

--- cli/index.js
+++ cli/index.js
@@ -2,7 +2,7 @@
 
 import axios from "axios";
 import express from "express";
-import { v4: uuidv4  } from "uuid";
+import { v4 as uuidv4 } from "uuid";
 import commandLineArgs from "command-line-args";
 import commandLineUsage from "command-line-usage";
 import ChromeLauncher from "chrome-launcher";
